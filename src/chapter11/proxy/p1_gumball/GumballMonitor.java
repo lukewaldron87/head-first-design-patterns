4da3bf00c75a1d4dadd436f6f90ca6a165fd45da
Luke Waldron <lukewaldron87@gmail.com>
2020-07-29 07:59:11 +0200

    Added monitors to the GumballMachine

---
+++ src/chapter11/proxy/p1_gumball/GumballMonitor.java
@@ -0,0 +1,21 @@
+package chapter11.proxy.p1_gumball;
+
+import java.rmi.RemoteException;
+
+public class GumballMonitor {
+    GumballMachineRemote machineRemote;
+
+    public GumballMonitor(GumballMachineRemote machineRemote){
+        this.machineRemote = machineRemote;
+    }
+
+    public void report(){
+        try{
+            System.out.println("Gumball Machine: " + machineRemote.getLocation());
+            System.out.println("Current inventory: " + machineRemote.getCount() + " gumballs");
+            System.out.println("Current state: " + machineRemote.getState());
+        }catch (RemoteException e){
+            e.printStackTrace();
+        }
+    }
+}
